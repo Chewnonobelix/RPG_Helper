8305170ffd25f970024fbb436a58b258fda8672d
Arnaud duhamel <duhamel@abvent.fr>
2020-08-12 09:48:41 +0200

    [Controllers]add interfacedata

---
+++ RpgDll/MainRpg/Controller/interfacedatasave.h
@@ -0,0 +1,21 @@
+#ifndef INTERFACEDATASAVE_H
+#define INTERFACEDATASAVE_H
+
+#include <QObject>
+
+#include "Bestiary/abstractcreature.h"
+
+class InterfaceDataSave: public QObject
+{
+public:
+    InterfaceDataSave() = default;
+    InterfaceDataSave(const InterfaceDataSave&) = delete;
+    ~InterfaceDataSave() = default;
+
+    virtual QMap<QUuid, CreaturePointer> selectCreature(QList<QUuid> = QList<QUuid>()) = 0;
+    virtual bool removeCreature(CreaturePointer) = 0;
+    virtual bool updateCreature(CreaturePointer) = 0;
+    virtual bool addCreature(CreaturePointer) = 0;
+
+};
+#endif // INTERFACEDATASAVE_H
