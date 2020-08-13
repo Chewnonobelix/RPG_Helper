3eaee249835dd278100787535ca8fcd4e1f22281
Arnaud duhamel <duhamel@abvent.fr>
2020-08-13 14:01:18 +0200

    [TU SQL] add init and add creature test

--- RpgDll/TestRpg/TestSql/tst_testsql.cpp
+++ RpgDll/TestRpg/TestSql/tst_testsql.cpp
@@ -1,33 +1,64 @@
 #include <QtTest>
+#include "Controller/sqldatabase.h"
 
 // add necessary includes here
 
 class TestSql : public QObject
 {
     Q_OBJECT
+private:
+    QSharedPointer<SqlDataBase> model;
 
 public:
     TestSql();
     ~TestSql();
 
 private slots:
-    void test_case1();
-
+    void initTestCase();
+    void test_init();
+    void test_addCreature();
+    void cleanupTestCase();
 };
 
 TestSql::TestSql()
 {
+
+}
+
+void TestSql::initTestCase()
+{
+    model.reset(new SqlDataBase);
     qDebug()<<QString(REQFILE)<<QFile::exists(QString(REQFILE));
+}
+
+void TestSql::cleanupTestCase()
+{
+    model.clear();
+    qDebug()<<QFile::remove("generic")<<QFile::exists("generic");
 }
 
 TestSql::~TestSql()
 {
-
 }
 
-void TestSql::test_case1()
+void TestSql::test_init()
 {
+    try
+    {
+        QCOMPARE(model->init(), true);
+    }
+    catch(QString error)
+    {
+        qDebug()<<error;
+        QFAIL("Exeption");
+    }
+}
 
+void TestSql::test_addCreature()
+{
+    auto c = AbstractCreature::createGeneric();
+    QCOMPARE(model->addCreature(c), true);
+    QVERIFY(!c->id().isNull());
 }
 
 QTEST_APPLESS_MAIN(TestSql)
