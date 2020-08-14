6dee0443c682f16e000aeaa3a43ce798c60dee79
Arnaud duhamel <duhamel@abvent.fr>
2020-08-14 16:20:13 +0200

    [Controllers] add qml type

--- RpgDll/MainRpg/Controller/maincontroller.cpp
+++ RpgDll/MainRpg/Controller/maincontroller.cpp
@@ -4,6 +4,9 @@ MainController::MainController()
 {
     setDb(new SqlDataBase);
     qmlRegisterUncreatableType<AbstractObject>("Bestiary.Generic", 1, 0, "AbstractObject", "Abstract type");
+    qmlRegisterUncreatableType<AbstractRule>("Bestiary.Generic", 1, 0, "AbstractRule", "Abstract type");
+    qmlRegisterUncreatableType<AbstractCreature>("Bestiary.Generic", 1, 0, "AbstractCreature", "Abstract type");
+    qmlRegisterUncreatableType<AbstractWeapon>("Bestiary.Generic", 1, 0, "AbstractWeapon", "Abstract type");
 
     m_engine.load(QUrl("qrc:/Main.qml"));
     auto root = m_engine.rootObjects().first();
